fd98d03390ee7235de538a4375965f412f15029b
Agus Purwantoro <37340703+theagusp@users.noreply.github.com>
2018-04-02 22:59:27 +0700

    final relase v1.0a

--- simple-ssh-tunnel.sh
+++ simple-ssh-tunnel.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, dEXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-HOST=$(zenity –entry –title=”Host” –text=”Enter a host.”)
-USER=$(zenity –entry –title=”User” –text=”Enter a username.”)
-PASSWORD=$(zenity –entry –title=”Password” –text=”Enter a password.”)
-PORT=$(zenity –entry –title=”Port” –text=”Enter a port.”)
-LISTEN=$(zenity –entry –title=”Listen” –text=”Enter a listen port.”)
+# Simple SSH Tunnel v1.0a built with zenity.
 
-plink -v -N -C -D $LISTEN $USER@$HOST -P $PORT -pw 1
+zenity --warning --title "Attention" --text='Beta testing, Something might be wrong!'
 
-# Mit License
+host=$(zenity --entry --title "Host" --text "Enter hostname. Eg: 127.0.0.1")
+port=$(zenity --entry --title "Port" --text "Enter server port. Eg: 443")
+user=$(zenity --entry --title "Username" --text "Enter username. Eg: matt")
+password=$(zenity --password --title "Password" --text "Enter password. Eg: matt")
+listen=$(zenity --entry --title "Listen Port" --text "Enter your listen port. Eg: 1080")
 
-# Version 1.0
+plink -v -N -C -D "$listen" "$user"@"$host" -P "$port" -pw "$password"
