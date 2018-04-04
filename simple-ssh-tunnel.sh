b5ebf97f6c5164a89cd34a27fc7e1c97a8b1d129
Agus Purwantoro <37340703+theagusp@users.noreply.github.com>
2018-04-04 20:35:49 +0700

    v1.0a final, no bug ty.

--- simple-ssh-tunnel.sh
+++ simple-ssh-tunnel.sh
@@ -2,12 +2,12 @@
 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-zenity --warning --title "Attention" --text='Final release v1.0a, simple gui no form.'
+zenity --warning --title "Information" --text="This is simple gui no form, more features added soon. \n v1.0a final no bug, ty."
 
-host=$(zenity --entry --title "Host" --text "Enter hostname. Eg: 127.0.0.1")
-port=$(zenity --entry --title "Port" --text "Enter server port. Eg: 443")
-user=$(zenity --entry --title "Username" --text "Enter username. Eg: matt")
-password=$(zenity --password --title "Password" --text "Enter password. Eg: matt")
-listen=$(zenity --entry --title "Listen Port" --text "Enter your listen port. Eg: 1080")
+host=$(zenity --entry --title "Host" --text "Enter server Hostname/IP. Eg: localhost/127.0.0.1")
+port=$(zenity --entry --title "Port" --text "Enter server Port. Eg: 22, 80, 443")
+user=$(zenity --entry --title "Username" --text "Enter your Username. Eg: matt")
+password=$(zenity --password --title "Password" --text ="")
+listen=$(zenity --entry --title "Listen Port" --text ="Enter prefered Listen Port. Eg: 1080, 1081, 100, etc")
 
-plink -N -C -D "$listen" "$user"@"$host" -P "$port" -pw "$password"
+plink -N -C "$user"@"$host" -P "$port" -pw "$password" -D "$listen"
